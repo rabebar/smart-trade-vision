5686f38c424f3dac3bd90994a431833125be0f76
Rabebar <rabe.bar.a74@gmail.com>
2025-12-13 02:45:56 +0200

    Force Update Admin

--- main.py
+++ main.py
@@ -212,3 +212,4 @@ def make_me_king(db: Session = Depends(get_db)):
     user.is_premium = True
     db.commit()
     return {"status": "success", "message": f"مبروك! {user.email} أصبح الآن المدير والرصيد مليون! 👑"}
+# UPDATE ADMIN ACCESS NOW
