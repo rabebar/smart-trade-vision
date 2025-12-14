896828ecbdf3819c6facf0830c06baae220046d1
Rabebar <rabe.bar.a74@gmail.com>
2025-12-14 13:27:32 +0200

    Force SSL for PostgreSQL connection

--- database.py
+++ database.py
@@ -11,10 +11,13 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    connect_args={
+        "sslmode": "require"
+    }
 )
 
-SessionLocal = sessionmaker(
+  SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
